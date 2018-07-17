64460ca96aba9327f3d3c1c0f6ed113ffc097fa6
Sam Marcus <sammarcus@users.noreply.github.com>
2018-07-17 15:21:48 -0400

    Update gems_spec.rb

--- spec/gems_spec.rb
+++ spec/gems_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "Selector Gems" do
     expect(css_file_contents).to include('#sand img'), "Hint: select #sand"
   end
 
-  it 'selects the image based on it being a sibling that follows immediately after the element with an id of ball (using adjacent sibling selector)' do
+  it 'selects the image based on it being a sibling that follows immediately after the element with an id of ocean (using adjacent sibling selector)' do
     expect(css_file_contents).to include('#ocean + img'), "Hint: add img to #ocean"
   end
 
